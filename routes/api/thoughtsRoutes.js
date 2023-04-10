e2947f5e352a0f093cb9cdb3ba17b43f3bafd2cd
Franksu <fsuriel@gmail.com>
2023-04-10 14:05:17 -0400

    create reaction is working

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -6,6 +6,8 @@ const {
     createThought,
     updateThought,
     deleteThought,
+    createReaction,
+    deleteReaction,
 } = require('../../controllers/thoughtController');
 
 // /api/thought
@@ -17,5 +19,11 @@ router
 .get(getSingleThought)
 .put(updateThought)
 .delete(deleteThought);
+
+// /api/thought/:thoughtId/reactions
+router
+.route('/:thoughtId/reactions')
+.post(createReaction)
+.delete(deleteReaction)
 
 module.exports = router
